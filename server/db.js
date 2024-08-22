b4fab3299f5c705894cc270bb7c706c02eab1768
richardnewell15 <richardnewell15@gmail.com>
2024-08-21 18:40:14 -0700

    Update for 2

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:EglfgUzbpdA2NBEP@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:igq5dDDJGvsR9rb9nrY9HRmg@172.21.162.39:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
